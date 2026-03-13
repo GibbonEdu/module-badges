656b1df45c5877d5205b1a6bfeb202f673fd8b26
Ali Alam <aalam@ichk.edu.hk>
2026-03-13 13:42:35 +0800

    Badges: Added comprehensive file upload tracking system to monitor and manage all file uploads across the system

--- Badges/version.php
+++ Badges/version.php
@@ -22,5 +22,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /**
  * Sets version information.
  */
-$moduleVersion = '2.9.04';
+$moduleVersion = '2.9.05';
 $coreVersion = '29.0.00';
